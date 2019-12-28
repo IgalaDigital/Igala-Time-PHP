09d63d3f652e8fe4803208e4467471546da81a44
dfiredeveloper <dfiredeveloper@gmail.com>
2019-12-28 10:57:03 +0000

    Added Igala Constants

---
+++ src/IgalaConstants.php
@@ -0,0 +1,40 @@
+<?php
+// Declare the Language Time Namespace
+namespace LanguageTime;
+
+class IgalaConstants
+{
+    const number1 = "oka";
+    const number2 = "éjì";
+    const number3 = "ẹ́ta";
+    const number4 = "ẹ́lẹ́";
+    const number5 = "ẹ́lu";
+    const number6 = "ẹ́fà";
+    const number7 = "ébìe";
+    const number8 = "ẹ́jọ";
+    const number9 = "ẹ́là";
+    const number10 = "ẹ́kwà";
+    const number11 = "ẹ́kwàká";
+    const number12 = "ẹ́kwéjì";
+    const number13 = "ẹ́kwétà";
+    const number14 = "ẹ́kwélẹ́";
+    const number15 = "ẹ́kwélu";
+    const number16 = "ẹ́kwéfa";
+    const number17 = "ẹ́kwébíẹ́";
+    const number18 = "ẹ́kwéjo";
+    const number19 = "ẹ́kwéla";
+    const number20 = "Ogwú";
+    const number21 = "Ogwúnyiowoka";
+    const number22 = "Ogwúnyiowomeji";
+    const number23 = "Ogwúnyiowometa";
+    const number24 = "Ogwúnyiowoele";
+    const number25 = "Ogwúnyiowomelu";
+    const number26 = "Ogwúnyiowomefa";
+    const number27 = "Ogwúnyiowomebie";
+    const number28 = "Ogwúnyiowomejo";
+    const number29 = "Ogwúnyiowomela";
+    const number30 = " úkberú ";
+    const TIME_POINTER = "Agogo ";
+    const AFTER_POINTER = " Tulẹ́ ";
+    const BEFORE_POINTER = " Ki ";
+}
