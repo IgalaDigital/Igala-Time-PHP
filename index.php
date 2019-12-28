99d35bc573c1c5ca88720c9cb5022a422bd4e419
dfiredeveloper <dfiredeveloper@gmail.com>
2019-12-28 10:49:37 +0000

    Added Index File

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__.'/src/English.php';
+require_once __DIR__.'/src/Igala.php';
+
+
+// Import English Time Class
+use LanguageTime\English;
+
+//Import Igala Time Class
+use LanguageTime\Igala;
+
+echo new Igala();
+// Agogo Méjì Tule Mìnìtì Mélu
+echo new English();
